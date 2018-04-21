4a4954ae203a3590bc9888d8db919d59014816ca
wwdablu <dev.soumyamob@gmail.com>
2018-04-20 21:58:57 -0700

    Initial checkin

---
+++ bubble/src/main/java/com/soumya/wwdablu/bubble/BubbleView.kt
@@ -0,0 +1,12 @@
+package com.soumya.wwdablu.bubble
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BubbleView : View {
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        //
+    }
+}
